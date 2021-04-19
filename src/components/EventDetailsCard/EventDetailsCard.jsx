632cbb9f1933064268fbeaeb546f88bde7166278
kbarne12 <kbarne12@gmail.com>
2021-04-19 13:51:23 -0400

    wth

---
+++ src/components/EventDetailsCard/EventDetailsCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import './EventDetailsCard.css'
+import Card from "react-bootstrap/Card"
+import Button from "react-bootstrap/Button"
+import EventDetails from '../../pages/EventDetails/EventDetails' //??????
+
+function EventDetailsCard(props) {
+	//{user, event, court, handleDeleteEvent}
+	// const { user, event, deleteEvent, court} = props
+	console.log("Hello?")
+	return (
+		<>
+		</>
+	)
+}
+export default EventDetailsCard;
